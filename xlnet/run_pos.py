d04e774702dacc8d54c3ab63858c47455049a7bb
Han Shao <hshao@oberlin.edu>
2019-11-19 23:30:13 -0500

    fix bug

--- xlnet/run_pos.py
+++ xlnet/run_pos.py
@@ -322,7 +322,7 @@ def input_fn_builder(features, is_training, drop_remainder):
 
         if is_training:
             d = d.repeat()
-            d = d.shuffle(buffer_size=100, seed=FLAGS.seed)
+            d = d.shuffle(buffer_size=100)
 
         d = d.batch(batch_size=batch_size, drop_remainder=drop_remainder)
         return d
