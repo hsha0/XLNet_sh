7a0634f2d1cb9e6d3cab6db513da8aaa106a9d33
Han Shao <hshao@oberlin.edu>
2019-11-24 12:38:49 -0500

    update race.sh

--- xlnet/race.sh
+++ xlnet/race.sh
@@ -8,10 +8,10 @@ INIT_CKPT_DIR='xlnet_cased_L-12_H-768_A-12'
 GS_ROOT=gs://xlnet_sh
 GS_INIT_CKPT_DIR=${GS_ROOT}/${INIT_CKPT_DIR}
 GS_PROC_DATA_DIR=${GS_ROOT}/proc_data/race
-GS_MODEL_DIR=${GS_ROOT}/experiment/race_epochs_2
+GS_MODEL_DIR=${GS_ROOT}/experiment/race_epochs2_lr2e-05
 
 # TPU name in google cloud
-TPU_NAME=grpc://10.72.145.162:8470
+TPU_NAME=grpc://10.50.7.130:8470
 
 python3 run_race.py \
   --use_tpu=True \
